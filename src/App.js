67556ff96f5a2e820d648b3a6366574e07443824
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-06-29 17:45:39 +0300

    moved items to their own file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React from "react";
+
+//data
+import books from "./items";
 const styles = {
   text: {
     textAlign: "center",
   },
-  size: {
+  pageImageSize: {
     width: "100%",
     height: "200px",
   },
+  background: {
+    backgroundColor: "yellow",
+  },
 };
 function App() {
+  const booklist = books.map((book) => (
+    <div style={styles.background} style={styles.text} key={book.id}>
+      <p>{book.name}</p>
+      <img src={book.image} alt={book.name} />
+      <p>{book.price}</p>
+    </div>
+  ));
   return (
-    <div>
+    <div style={styles.background}>
       <h1 style={styles.text}>My Books</h1>
       <h2>
         Here you can read my books, therefore publishers are not required, ano
         not allowed to read my stuff too!
       </h2>
       <img
-        style={styles.size}
+        style={styles.pageImageSize}
         src="https://media.wired.com/photos/5be4cd03db23f3775e466767/master/w_2560%2Cc_limit/books-521812297.jpg"
         alt="storeimage"
       />
-      <div style={styles.text}>
+      <div>{booklist}</div>
+      {/* <div style={styles.text}>
         <h1>خلايا الزمرد</h1>
         <img
           src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1479637297l/33017563._SX318_.jpg"
@@ -44,7 +58,7 @@ function App() {
           alt="book3 image"
         />
         <h3>1.00 KD</h3>
-      </div>
+      </div> */}
     </div>
   );
 }
---
+++ src/items.js
@@ -0,0 +1,24 @@
+const items = [
+  {
+    id: 1,
+    name: "خلايا الزمرد",
+    image:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1479637297l/33017563._SX318_.jpg",
+    price: 3,
+  },
+  {
+    id: 2,
+    name: "أوتار الرنين",
+    image:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1509109523l/36486018._SX318_.jpg",
+    price: 2,
+  },
+  {
+    id: 3,
+    name: "صانع الدمى - طوق الأحلام",
+    image:
+      "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1510327420l/36570178._SX318_.jpg",
+    price: 1,
+  },
+];
+export default items;
